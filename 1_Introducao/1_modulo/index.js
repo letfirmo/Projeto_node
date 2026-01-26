5f37eb5659f022350c3631d54d6b04b0666b8d64
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2026-01-26 20:34:19 -0300

    feat: first class

---
+++ 1_Introducao/1_modulo/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs') //File System
+
+fs.readFile('file.txt', 'utf8', (err, data) =>{
+
+    if(err) {
+        console.log(err)
+        return
+    }
+
+    console.log(data)
+
+})
