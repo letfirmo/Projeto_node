69cecf2da8353af9dd5cda28cebf9efdf41ba72a
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2026-01-26 21:25:41 -0300

    feat: internal and external modules

---
+++ 2_fundamentals/1_internal-module/index.js
@@ -0,0 +1,5 @@
+const myModule = require('./my-module');
+const soma = myModule.soma;
+
+soma(2,19);
+soma(5,15);
---
+++ 2_fundamentals/3_read-arguments/index.js
@@ -0,0 +1,17 @@
+//name
+
+console.log(process.argv);
+
+const args = process.argv.slice(2);
+
+console.log(args);
+
+const name = args[0].split('=')[1];
+
+console.log(name);
+
+const age = args[1].split('=')[1];
+
+console.log(age);
+
+console.log(`Her name is ${name} and she is ${age} years old.`)
