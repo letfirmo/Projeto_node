eda274463009aee815d7b86c977018d3cd6093ff
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2026-01-27 21:21:52 -0300

    feat: input abstration

---
+++ 2_fundamentals/8_input-abstraction/index.js
@@ -0,0 +1,19 @@
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+            {
+                name: 'p1',
+                message: 'What is the first grade?',
+            }, 
+            {
+                name: 'p2',
+                message: 'What is the second grade?',
+            }
+        ])
+        .then((answers) => {
+            console.log(answers)
+            const media = ((parseInt(answers.p1) + parseInt(answers.p2)) / 2);
+            console.log(`The average is: ${media}`);
+        })
+        .catch((err) => console.log(err));
